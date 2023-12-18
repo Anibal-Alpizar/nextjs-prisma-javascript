21a3d81f2fafbbb866c3963ee93b45e61342e09b
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-18 00:03:58 -0600

    added: react form

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,5 +1,59 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 function NewPage() {
-  return <div>Tasks form</div>;
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+    console.log(title, description);
+    const res = await fetch(`/api/tasks`, {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    router.push("/");
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <form
+        className="bg-slate-800 p-10 lg:w-1/4 md:w-1/2"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="title" className="font-bold text-sm">
+          Task Title
+        </label>
+        <input
+          id="title"
+          type="text"
+          placeholder="Title Task"
+          className="border bg-gray-400 p-2 mb-4 w-full text-black"
+        />
+        <label htmlFor="description" className="font-bold text-sm">
+          Task Description
+        </label>
+        <textarea
+          id="description"
+          placeholder="Description Task"
+          rows="3"
+          className="border bg-gray-400 p-2 mb-4 w-full text-black"
+        ></textarea>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Create
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default NewPage;
