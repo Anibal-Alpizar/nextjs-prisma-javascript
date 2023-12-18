0f295b532a13eeb286e4cbc7594abd5912c8c829
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-18 00:22:15 -0600

    tasks render

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,35 @@
-function HomePage() {
-  return <div>Tasks</div>;
+import { prisma } from "@/libs/prisma";
+
+async function loadTasks() {
+  // using nextjs api
+  // const res = await fetch(`http://localhost:3000/api/tasks`);
+  // const data = await res.json();
+  // console.log(data);
+
+  // using prisma
+  const tasks = await prisma.task.findMany();
+  console.log(tasks);
+  return tasks;
+}
+
+async function HomePage() {
+  const tasks = await loadTasks();
+  return (
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3 mt-10">
+        {tasks.map((task) => (
+          <div
+            key={task.id}
+            className="bg-slate-900 p-3 hover:bg-sky-800 hover:cursor-pointer"
+          >
+            <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
+            <p>{task.description}</p>
+            <p>{new Date(task.createdAt).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default HomePage;
