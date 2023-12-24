2718b7564a20da491373e6e1433219f0b9438c17
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-23 20:29:55 -0600

    added: update tasks

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,23 +1,52 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-function NewPage() {
+function NewPage({ params }) {
+  console.log(params);
   const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    if (params.id) {
+      fetch(`/api/tasks/${params.id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setTitle(data.title);
+          setDescription(data.description);
+        });
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const title = e.target.title.value;
-    const description = e.target.description.value;
-    console.log(title, description);
-    const res = await fetch(`/api/tasks`, {
-      method: "POST",
-      body: JSON.stringify({ title, description }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await res.json();
-    console.log(data);
+    if (params.id) {
+      const res = await fetch(`/api/tasks/${params.id}`, {
+        method: "PUT",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+    } else {
+      const title = e.target.title.value;
+      const description = e.target.description.value;
+      console.log(title, description);
+      const res = await fetch(`/api/tasks`, {
+        method: "POST",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+    }
+    router.refresh();
     router.push("/");
   };
 
@@ -34,6 +63,8 @@ function NewPage() {
           id="title"
           type="text"
           placeholder="Title Task"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
           className="border bg-gray-400 p-2 mb-4 w-full text-black"
         />
         <label htmlFor="description" className="font-bold text-sm">
@@ -43,6 +74,8 @@ function NewPage() {
           id="description"
           placeholder="Description Task"
           rows="3"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
           className="border bg-gray-400 p-2 mb-4 w-full text-black"
         ></textarea>
         <button
